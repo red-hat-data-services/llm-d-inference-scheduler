87e3e0e79bd149674f2bcbf03e809df405908d55
Andres Llausas <allausas@redhat.com>
2025-11-11 13:16:35 -0500

    RHOAIENG-38498 Fixing conforma issues
    Signed-off-by: Andres Llausas <allausas@redhat.com>

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+// Minimal library file required by Cargo.toml
+// This file exists to allow cargo vendor to work during the prefetch step
+// The actual tokenizers library is built from source in the Dockerfile
+
